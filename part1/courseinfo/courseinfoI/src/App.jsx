270b45bc2342d6e1a6c47c2a398d484a5cbdc50c
Muhozgu <muhozgu_aehit@students.vizja.pl>
2025-03-06 21:03:24 +0100

    create 3 components

--- part1/courseinfo/courseinfoI/src/App.jsx
+++ part1/courseinfo/courseinfoI/src/App.jsx
@@ -1,3 +1,27 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  )
+}
+
+const Content = () => {
+  return (
+    <div>
+      <p>{part1} {exercises1}</p>
+      <p>{part2} {exercises2}</p>
+      <p>{part3} {exercises3}</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +33,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course='course'/>
+      <Content part1='part1' part2='part2' part3='part3'/>
+      <Footer exercises1='exercises1' exercises2='exercises2' exercises3='exercises3'/>
     </div>
   )
 }
